4f0bc7c180617074daa443ce7dabdc9e79c6d8da
mohamedali <soft.mohamedali@gmail.com>
2023-02-11 23:32:47 +0200

    test actor mapper create connectivity observer utility

--- core/src/main/java/com/example/core/data/connectivity/NetworkConnectivityObserver.kt
+++ core/src/main/java/com/example/core/data/connectivity/NetworkConnectivityObserver.kt
@@ -1,10 +1,52 @@
 package com.example.core.data.connectivity
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.Network
+import android.os.Build
+import androidx.annotation.RequiresApi
 import com.example.core.domain.connectivity.ConnectivityObserver
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.launch
 
-class NetworkConnectivityObserver:ConnectivityObserver {
+class NetworkConnectivityObserver(context:Context):ConnectivityObserver {
+
+    val connectivityManger=context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    @OptIn(ExperimentalCoroutinesApi::class)
     override fun observer(): Flow<ConnectivityObserver.ConnectivityStatus> {
+        return  callbackFlow {
+            val callBack=object:ConnectivityManager.NetworkCallback(){
+                override fun onAvailable(network: Network) {
+                    super.onAvailable(network)
+                    launch { send(ConnectivityObserver.ConnectivityStatus.AVAILABLE) }
+                }
 
+                override fun onLosing(network: Network, maxMsToLive: Int) {
+                    super.onLosing(network, maxMsToLive)
+                    launch { send(ConnectivityObserver.ConnectivityStatus.ISLOSING) }
+                }
+
+                override fun onLost(network: Network) {
+                    super.onLost(network)
+                    launch { send(ConnectivityObserver.ConnectivityStatus.LOST) }
+                }
+
+                override fun onUnavailable() {
+                    super.onUnavailable()
+                    launch { send(ConnectivityObserver.ConnectivityStatus.UNAVAILABLE) }
+                }
+
+            }
+            connectivityManger.registerDefaultNetworkCallback(callBack)
+            awaitClose {
+                connectivityManger.unregisterNetworkCallback(callBack)
+            }
+        }.distinctUntilChanged()
     }
 }
