a4c5a6593e4aaf88254c8f7b2dbdc83b5a9e549c
mohamedali <soft.mohamedali@gmail.com>
2023-03-14 00:28:47 +0200

    refactor code to be applicable for testing and write testing ActorViewModelTest.kt

---
+++ movies/movie_domin/src/test/java/com/example/domin/usecases/remote/GetPopularActorUseCaseTest.kt
@@ -0,0 +1,17 @@
+package com.example.domin.usecases.remote
+
+import org.junit.Assert.*
+
+import org.junit.After
+import org.junit.Before
+
+class GetPopularActorUseCaseTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+}
